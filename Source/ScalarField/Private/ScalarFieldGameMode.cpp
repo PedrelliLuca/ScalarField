604d24b5ca47664e26f9eb67b0a7d710c5cac4e1
Luca Pedrelli <pedrelli.luca@gmail.com>
2022-11-20 20:18:01 +0100

    Removal of old character class

--- Source/ScalarField/Private/ScalarFieldGameMode.cpp
+++ Source/ScalarField/Private/ScalarFieldGameMode.cpp
@@ -2,16 +2,14 @@
 
 #include "ScalarFieldGameMode.h"
 #include "ScalarFieldPlayerController.h"
-#include "ScalarFieldCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
-AScalarFieldGameMode::AScalarFieldGameMode()
-{
+AScalarFieldGameMode::AScalarFieldGameMode() {
 	// use our custom PlayerController class
 	PlayerControllerClass = AScalarFieldPlayerController::StaticClass();
 
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter_2"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
 	if (PlayerPawnBPClass.Class != nullptr)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
