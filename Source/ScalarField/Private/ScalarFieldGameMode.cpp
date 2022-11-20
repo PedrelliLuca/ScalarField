8c2a5bad385c2865019c214ac1bf7633f66feaaa
Luca Pedrelli <pedrelli.luca@gmail.com>
2022-11-20 20:22:25 +0100

    Old thermo actor class removal

--- Source/ScalarField/Private/ScalarFieldGameMode.cpp
+++ Source/ScalarField/Private/ScalarFieldGameMode.cpp
@@ -10,15 +10,13 @@ AScalarFieldGameMode::AScalarFieldGameMode() {
 
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != nullptr)
-	{
+	if (PlayerPawnBPClass.Class != nullptr) {
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 
 	// set default controller to our Blueprinted controller
 	static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/TopDown/Blueprints/BP_TopDownPlayerController"));
-	if(PlayerControllerBPClass.Class != NULL)
-	{
+	if (PlayerControllerBPClass.Class != NULL) {
 		PlayerControllerClass = PlayerControllerBPClass.Class;
 	}
 }
