a59600559695c8b6368176b1070302c264a8eabf
Luca Pedrelli <pedrelli.luca@gmail.com>
2022-11-26 20:42:26 +0100

    Interaction System module

---
+++ Source/InteractionSystem/InteractionSystem.Build.cs
@@ -0,0 +1,34 @@
+//reference on UE4 BlankModule https://docs.unrealengine.com/en-US/API/Developer/BlankModule/IBlankModule/index.html
+
+namespace UnrealBuildTool.Rules
+{
+	public class InteractionSystem : ModuleRules
+	{
+		public InteractionSystem(ReadOnlyTargetRules Target) : base(Target)
+		{
+            PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+            
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"Core",
+					// ... add other public dependencies that you statically link with here ...
+				}
+				);
+
+			PrivateDependencyModuleNames.AddRange(
+				new string[]
+				{
+					// ... add private dependencies that you statically link with here ...
+				}
+				);
+
+			DynamicallyLoadedModuleNames.AddRange(
+				new string[]
+				{
+					// ... add any modules that your module loads dynamically here ...
+				}
+				);
+		}
+	}
+}
