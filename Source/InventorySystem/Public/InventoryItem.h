4b5cc8e51deec30fbd2adfcbddab453e820f9587
Luca Pedrelli <pedrelli.luca@gmail.com>
2022-12-10 10:30:45 +0100

    InventoryItem class creation

---
+++ Source/InventorySystem/Public/InventoryItem.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "InventoryItem.generated.h"
+
+// EditInlineNew? DefaultToInstanced?
+UCLASS(Blueprintable)
+class INVENTORYSYSTEM_API UInventoryItem : public UObject {
+     GENERATED_BODY()
+     
+public:
+};
