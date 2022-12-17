f79df7edd7adfbe702374a52d270e7c3b607efab
Luca Pedrelli <pedrelli.luca@gmail.com>
2022-12-17 15:21:05 +0100

    InventoryItemWidget class

---
+++ Source/InventoryCore/Public/InventoryItemWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Blueprint/UserWidget.h"
+#include "InventoryComponent.h"
+
+#include "InventoryItemWidget.generated.h"
+
+UCLASS()
+class INVENTORYCORE_API UInventoryItemWidget : public UUserWidget {
+     GENERATED_BODY()
+  
+public:
+};
