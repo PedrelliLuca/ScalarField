cac2f001bdbbb00050ed2e20ac31d8b7b658a1f3
Luca Pedrelli <pedrelli.luca@gmail.com>
2023-01-03 22:27:31 +0100

    Filter buttons, capacities on screen

--- Source/InventoryPresenter/Private/InventoryWidget.cpp
+++ Source/InventoryPresenter/Private/InventoryWidget.cpp
@@ -10,16 +10,27 @@ void UInventoryWidget::SetInventory(TWeakInterfacePtr<IInventory> inventory) {
 
 	check(inventory.IsValid());
 	_inventory = MoveTemp(inventory);
-	_inventory->OnInventoryUpdated().AddUObject(this, &UInventoryWidget::_resetInventoryItems);
+	_inventory->OnInventoryUpdated().AddUObject(this, &UInventoryWidget::_refreshInventoryItems);
 
-	_resetInventoryItems();
+	_currentClassFilter = nullptr;
+	_refreshInventoryItems();
 }
 
-void UInventoryWidget::_resetInventoryItems() {
+void UInventoryWidget::_filterItemsByClass(TSubclassOf<UObject> itemClass /*= nullptr*/) {
 	check(IsValid(_itemWidgetClass));
 	_inventoryItemsBox->ClearChildren();
 
+#if DO_CHECK
+	if (IsValid(itemClass)) {
+		check(itemClass->ImplementsInterface(UItem::StaticClass()));
+	}
+#endif
+	
 	for (const auto& item : _inventory->GetItems()) {
+		if (IsValid(itemClass) && !item.GetObject()->IsA(itemClass)) {
+			continue;
+		}
+		
 		const auto itemWidget = CreateWidget<UInventoryItemWidget>(GetOwningPlayer(), _itemWidgetClass);
 		itemWidget->SetItem(item);
 		itemWidget->OnItemUsage().AddUObject(this, &UInventoryWidget::_onItemBeingUsed);
@@ -27,6 +38,15 @@ void UInventoryWidget::_resetInventoryItems() {
 
 		_inventoryItemsBox->AddChildToWrapBox(itemWidget);
 	}
+
+	_currentClassFilter = MoveTemp(itemClass);
+
+	_refreshInventoryInformation();
+}
+
+
+void UInventoryWidget::_refreshInventoryItems() {
+	_filterItemsByClass(_currentClassFilter);
 }
 
 void UInventoryWidget::_onItemBeingUsed(TWeakInterfacePtr<IItem> item) {
--- Source/InventoryPresenter/Public/InventoryWidget.h
+++ Source/InventoryPresenter/Public/InventoryWidget.h
@@ -17,13 +17,21 @@ class INVENTORYPRESENTER_API UInventoryWidget : public UUserWidget, public IItem
 public:
      void SetInventory(TWeakInterfacePtr<IInventory> inventory) override;
 
+     UFUNCTION(BlueprintPure)
+     TScriptInterface<IInventory> GetInventory() const { return _inventory.GetObject(); }
 
 protected:
      UPROPERTY(EditDefaultsOnly, NoClear, Category = "Inventory Widget")
      TSubclassOf<UInventoryItemWidget> _itemWidgetClass = nullptr;
 
+     UFUNCTION(BlueprintCallable)
+     void _filterItemsByClass(TSubclassOf<UObject> itemClass = nullptr);
+
+     UFUNCTION(BlueprintImplementableEvent)
+     void _refreshInventoryInformation();
+     
 private:
-     void _resetInventoryItems();
+     void _refreshInventoryItems();
 
      void _onItemBeingUsed(TWeakInterfacePtr<IItem> item);
      void _onItemBeingDropped(TWeakInterfacePtr<IItem> item);
@@ -32,4 +40,7 @@ private:
      TObjectPtr<UWrapBox> _inventoryItemsBox;
 
      TWeakInterfacePtr<IInventory> _inventory = nullptr;
+
+     UPROPERTY()
+     TSubclassOf<UObject> _currentClassFilter = nullptr;
 };
