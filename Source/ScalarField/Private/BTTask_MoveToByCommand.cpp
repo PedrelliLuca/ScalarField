d6485ad1c688ff005ae3f8b799c8e646ef0179ae
Luca Pedrelli <Luca.Pedrelli@zuru.com>
2023-02-18 12:36:34 +0100

    Custom move task: AI Move Request preparation
    Taking inspiration from BTTask_MoveTo

--- Source/ScalarField/Private/BTTask_MoveToByCommand.cpp
+++ Source/ScalarField/Private/BTTask_MoveToByCommand.cpp
@@ -4,20 +4,68 @@
 #include "BTTask_MoveToByCommand.h"
 
 #include "AIMovementCommandComponent.h"
-#include "BehaviorTree/Blackboard/BlackboardKeyType_Vector.h"
+#include "AISystem.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Vector.h"
+#include "VisualLogger/VisualLogger.h"
+
 
 UBTTask_MoveToByCommand::UBTTask_MoveToByCommand() {
 	NodeName = "Move To By Command";
+	INIT_TASK_NODE_NOTIFY_FLAGS();
 
-	// Accept only actors and vectors
-	// BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_MoveToByCommand, BlackboardKey), AActor::StaticClass());
+	_acceptableRadius = GET_AI_CONFIG_VAR(AcceptanceRadius);
+	_bReachTestIncludesGoalRadius = _bReachTestIncludesAgentRadius = GET_AI_CONFIG_VAR(bFinishMoveOnGoalOverlap);
+	_bAllowStrafe = GET_AI_CONFIG_VAR(bAllowStrafing);
+	_bAllowPartialPath = GET_AI_CONFIG_VAR(bAcceptPartialPaths);
+	_bTrackMovingGoal = true;
+	_bProjectGoalLocation = true;
+	_bUsePathfinding = true;
+
+	_bStopOnOverlapNeedsUpdate = true;
+
+	_observedBlackboardValueTolerance = _acceptableRadius * 0.95f;
+
+	// Only Actors and Vectors are accepted
+	BlackboardKey.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_MoveToByCommand, BlackboardKey), AActor::StaticClass());
 	BlackboardKey.AddVectorFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_MoveToByCommand, BlackboardKey));
 }
 
 EBTNodeResult::Type UBTTask_MoveToByCommand::ExecuteTask(UBehaviorTreeComponent& ownerComp, uint8* nodeMemory) {
+	const auto memory = CastInstanceNodeMemory<FBTMoveToByCommandTaskMemory>(nodeMemory);
+	memory->PreviousGoalLocation = FAISystem::InvalidLocation;
+	memory->MoveRequestID = FAIRequestID::InvalidRequest;
+
 	const auto aiController = ownerComp.GetAIOwner();
-	const auto aiMovementCommandC = aiController->FindComponentByClass<UAIMovementCommandComponent>();
+	check(IsValid(aiController));
+
+	auto nodeResult = _performMoveTask(ownerComp, nodeMemory);
+
+	
+}
+
+void UBTTask_MoveToByCommand::PostLoad() {
+	Super::PostLoad();
+
+	if (_bStopOnOverlapNeedsUpdate) {
+		_bStopOnOverlapNeedsUpdate = false;
+		_bReachTestIncludesAgentRadius = _bStopOnOverlap;
+		_bReachTestIncludesGoalRadius = false;
+	}
+}
+
+#if WITH_EDITOR
+
+void UBTTask_MoveToByCommand::OnNodeCreated() {
+	Super::OnNodeCreated();
+	// _bStopOnOverlapNeedsUpdate = false;
+}
+
+#endif
+
+EBTNodeResult::Type UBTTask_MoveToByCommand::_performMoveTask(UBehaviorTreeComponent& onwerComp, uint8* nodeMemory) {
+	/*const auto aiMovementCommandC = aiController->FindComponentByClass<UAIMovementCommandComponent>();
 	if (!IsValid(aiMovementCommandC)) {
 		UE_LOG(LogTemp, Error, TEXT("%s(): AI Controller does not have a Movement Command Component"), *FString{ __FUNCTION__ });
 		return EBTNodeResult::Failed;
@@ -30,5 +78,44 @@ EBTNodeResult::Type UBTTask_MoveToByCommand::ExecuteTask(UBehaviorTreeComponent&
 	aiMovementCommandC->GetMovementCommand()->OnStopMovement(aiController);
 	aiMovementCommandC->GetMovementCommand()->OnSetDestination(aiController, targetLocation);
 
-	return EBTNodeResult::Succeeded;
+	return EBTNodeResult::Succeeded;*/
+	
+	auto nodeResult = EBTNodeResult::Failed;
+
+	const auto blackboardC = onwerComp.GetBlackboardComponent();
+	const auto aiController = onwerComp.GetAIOwner();
+
+	if (!IsValid(aiController) || !IsValid(blackboardC)) {
+		return nodeResult;
+	}
+
+	// Move Request preparation
+	FAIMoveRequest moveReq;
+	moveReq.SetNavigationFilter(*_filterClass ? _filterClass : aiController->GetDefaultNavigationFilterClass());
+	moveReq.SetAllowPartialPath(_bAllowPartialPath);
+	moveReq.SetAcceptanceRadius(_acceptableRadius);
+	moveReq.SetCanStrafe(_bAllowStrafe);
+	moveReq.SetReachTestIncludesAgentRadius(_bReachTestIncludesAgentRadius);
+	moveReq.SetReachTestIncludesGoalRadius(_bReachTestIncludesGoalRadius);
+	moveReq.SetProjectGoalLocation(_bProjectGoalLocation);
+	moveReq.SetUsePathfinding(_bUsePathfinding);
+
+	const auto myMemory = CastInstanceNodeMemory<FBTMoveToByCommandTaskMemory>(nodeMemory);
+
+	if (BlackboardKey.SelectedKeyType == UBlackboardKeyType_Object::StaticClass()) {
+		const auto keyValue = blackboardC->GetValue<UBlackboardKeyType_Object>(BlackboardKey.GetSelectedKeyID());
+		const auto targetActor = Cast<AActor>(keyValue);
+		if (IsValid(targetActor)) {
+			_bTrackMovingGoal ? moveReq.SetGoalActor(targetActor) : moveReq.SetGoalLocation(targetActor->GetActorLocation());
+		} else {
+			UE_VLOG(aiController, LogBehaviorTree, Warning, TEXT("%s(): Tried to go to actor while BB %s entry was empty"), *FString{__FUNCTION__}, *BlackboardKey.SelectedKeyName.ToString());
+		}
+	} else if (BlackboardKey.SelectedKeyType == UBlackboardKeyType_Vector::StaticClass()) {
+		const auto targetLocation = blackboardC->GetValue<UBlackboardKeyType_Vector>(BlackboardKey.GetSelectedKeyID());
+		moveReq.SetGoalLocation(targetLocation);
+		myMemory->PreviousGoalLocation = targetLocation;
+	} else {
+		// Constructor says only Actors and Vectors are accepted, something horrible is happening
+		checkNoEntry();
+	}
 }
--- Source/ScalarField/Public/BTTask_MoveToByCommand.h
+++ Source/ScalarField/Public/BTTask_MoveToByCommand.h
@@ -5,8 +5,21 @@
 #include "CoreMinimal.h"
 
 #include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "Tasks/AITask_MoveTo.h"
 
 #include "BTTask_MoveToBycommand.generated.h"
+
+struct FBTMoveToByCommandTaskMemory {
+	/** Move request ID */
+	FAIRequestID MoveRequestID;
+
+	FDelegateHandle BBObserverDelegateHandle;
+	FVector PreviousGoalLocation;
+
+	TWeakObjectPtr<UAITask_MoveTo> Task;
+
+	uint8 bObserverCanFinishTask : 1;
+};
 
 /**
  * 
@@ -20,5 +33,59 @@ public:
 	
 private:
 	EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& ownerComp, uint8* nodeMemory) override;
+
+	void PostLoad() override;
+
+#if WITH_EDITOR
+	virtual void OnNodeCreated() override;
+#endif 
 	
+	EBTNodeResult::Type _performMoveTask(UBehaviorTreeComponent& onwerComp, uint8* nodeMemory);
+
+	/** if set, radius of goal's capsule will be added to threshold between AI and goal location in destination reach test  */
+	UPROPERTY(Category = Node, EditAnywhere)
+	uint32 _bReachTestIncludesGoalRadius : 1;
+	
+	/** if set, radius of AI's capsule will be added to threshold between AI and goal location in destination reach test  */
+	UPROPERTY(Category = Node, EditAnywhere)
+	uint32 _bReachTestIncludesAgentRadius : 1;
+
+	/** if set, move will use pathfinding. Not exposed on purpose, please use BTTask_MoveDirectlyToward */
+	uint32 _bUsePathfinding : 1;
+
+	UPROPERTY(Category = Node, EditAnywhere)
+	uint32 _bAllowStrafe : 1;
+
+	/** if set, use incomplete path when goal can't be reached */
+	UPROPERTY(Category = Node, EditAnywhere, AdvancedDisplay)
+	uint32 _bAllowPartialPath : 1;
+
+	/** if set, path to goal actor will update itself when actor moves */
+	UPROPERTY(Category = Node, EditAnywhere, AdvancedDisplay)
+	uint32 _bTrackMovingGoal : 1;
+
+	/** if set, goal location will be projected on navigation data (navmesh) before using */
+	UPROPERTY(Category = Node, EditAnywhere, AdvancedDisplay)
+	uint32 _bProjectGoalLocation : 1;
+
+	/** if task is expected to react to changes to location represented by BB key 
+	 *	this property can be used to tweak sensitivity of the mechanism. Value is 
+	 *	recommended to be less than AcceptableRadius */
+	UPROPERTY(Category=Blackboard, EditAnywhere, meta = (ClampMin = "1", UIMin = "1", EditCondition="bObserveBlackboardValue", DisplayAfter="bObserveBlackboardValue"))
+	float _observedBlackboardValueTolerance;
+
+	/** DEPRECATED, please use combination of bReachTestIncludes*Radius instead */
+	UPROPERTY(Category = Node, VisibleInstanceOnly)
+	uint32 _bStopOnOverlap : 1;
+	
+	UPROPERTY()
+	uint32 _bStopOnOverlapNeedsUpdate : 1;
+
+	/** fixed distance added to threshold between AI and goal location in destination reach test */
+	UPROPERTY(config, Category = Node, EditAnywhere, meta=(ClampMin = "0.0", UIMin="0.0"))
+	float _acceptableRadius;
+
+	/** "None" will result in default filter being used */
+	UPROPERTY(Category = Node, EditAnywhere)
+	TSubclassOf<UNavigationQueryFilter> _filterClass;
 };
