2d572546a380f5111f2f3b37835336df16a1cf66
Luca Pedrelli <Luca.Pedrelli@zuru.com>
2023-06-01 15:32:49 +0200

    PlayerInputData description

--- Source/MovementCommand/Public/PlayerInputData.h
+++ Source/MovementCommand/Public/PlayerInputData.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 
+/** \brief The logic of the Player Movement Command might depend on how the input was provided. This struct stores the information on the Player's input to be
+ * provided to the command. */
 struct FPlayerInputData {
     EInputEvent SetDestinationInputEvent{};
 };
