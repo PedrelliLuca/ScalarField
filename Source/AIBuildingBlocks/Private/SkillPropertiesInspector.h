395d6fdacb11af06ed82747cd6b3301390551459
Luca Pedrelli <Luca.Pedrelli@zuru.com>
2023-06-01 15:22:59 +0200

    Inspectors' documentation

--- Source/AIBuildingBlocks/Private/SkillPropertiesInspector.h
+++ Source/AIBuildingBlocks/Private/SkillPropertiesInspector.h
@@ -6,6 +6,16 @@
 
 #include "NewAbstractSkill.h"
 
+/** \brief This class is friend of UNewAbstractSkill. Files who have visibility of this class are allowed to peek at UNewAbstractSkill's properties. The reasons
+ * this class exists are:
+* 1. To provide AI with skill-related data, so that it can take decisions on whether to cast a certain skill or not.
+ * 2. To provide UI with skill-related data, so that the player can take decisions on whether to cast a certain skill or not.
+ *
+ *
+ * IMPORTANT: THIS CLASS SHOULD BE UNKNOWN BY EVERY FILE THAT IS NOT AI OR UI RELATED.
+ *
+ * TODO: move this in a more abstract module when the skills-related UI will be implemented.
+ */
 class FSkillPropertiesInspector {
 public:
     FSkillPropertiesInspector(const TObjectPtr<UNewAbstractSkill>& skill)
--- Source/AIBuildingBlocks/Private/SkillsContainerInspector.h
+++ Source/AIBuildingBlocks/Private/SkillsContainerInspector.h
@@ -8,6 +8,15 @@
 #include "NewSkillsContainerComponent.h"
 #include "SkillPropertiesInspector.h"
 
+/** \brief This class is friend of UNewSkillsContainerComponent. Files who have visibility of this class are allowed to peek at UNewSkillsContainerComponent's
+ * properties. The reasons this class exists are:
+ * 1. To give AI knowledge on all the possible skills at its disposal, so that it can take decisions on what to do.
+ * 2. To have the UI w all the possible skills at its disposal, so that the player can take decisions on what to do.
+ *
+ * IMPORTANT: THIS CLASS SHOULD BE UNKNOWN BY EVERY FILE THAT IS NOT AI OR UI RELATED.
+ *
+ * TODO: move this in a more abstract module when the skills-related UI will be implemented.
+ */
 class FSkillsContainerInspector {
 public:
     FSkillsContainerInspector(const TObjectPtr<UNewSkillsContainerComponent>& skillsContainer)
