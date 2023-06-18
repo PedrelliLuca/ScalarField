267ad2fbca7fc921cb1017bc8f2f460b3fc85e84
Luca Pedrelli <Luca.Pedrelli@zuru.com>
2023-06-18 18:44:35 +0200

    DoorKeyItem class

---
+++ Source/ConcreteInteractables/Private/Doors/DoorKeyItem.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Doors/DoorKeyItem.h"
+
+#define LOCTEXT_NAMESPACE "DoorKeyItem"
+
+UDoorKeyItem::UDoorKeyItem() {
+    _bDoesUseConsume = false;
+}
+
+#undef LOCTEXT_NAMESPACE
