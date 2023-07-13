928c383b0045fe4e67f3876581e88e27238436a4
Luca Pedrelli <Luca.Pedrelli@zuru.com>
2023-07-13 14:53:29 +0200

    End level trigger

---
+++ Source/ScalarField/Public/EndLevelTrigger.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "EndLevelTrigger.generated.h"
+
+/**
+ *
+ */
+UCLASS(Blueprintable)
+class SCALARFIELD_API AEndLevelTrigger : public ATriggerBox {
+    GENERATED_BODY()
+
+protected:
+    void BeginPlay() override;
+    
+private:
+    UFUNCTION()
+    void _onPlayerOverlap(AActor* overlappedActor, AActor* otherActor);
+};
