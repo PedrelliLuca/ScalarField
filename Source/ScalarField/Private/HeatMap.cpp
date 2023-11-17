0493579aea08f54f152b2fff4d0e5f6f663a4a10
Luca Pedrelli <pedrelli.luca@gmail.com>
2023-11-17 20:55:33 +0000

    2x2 px texture

--- Source/ScalarField/Private/HeatMap.cpp
+++ Source/ScalarField/Private/HeatMap.cpp
@@ -44,6 +44,11 @@ void AHeatMap::BeginPlay() {
 
     // This texture has B = 1 for the first half. We don't give a shit about the other values, this is just to mask the color for a given region.
     UTexture2D* const topTexture = UTexture2D::CreateTransient(_width, _height);
+
+    // topTexture->MipGenSettings = TextureMipGenSettings::TMGS_NoMipmaps;
+    // topTexture->GetTextureGroupString useless?
+    topTexture->Filter = TextureFilter::TF_Nearest;
+
     FTexture2DMipMap* const topMipMap = &topTexture->PlatformData->Mips[0];
     FByteBulkData* const topImageData = &topMipMap->BulkData;
     const int64 topSize = topImageData->GetBulkDataSize();
@@ -52,27 +57,53 @@ void AHeatMap::BeginPlay() {
     // [Pixel1 B][Pixel1 G][Pixel1 R][Pixel1 A][Pixel2 B][Pixel2 G][Pixel2 R][Pixel2 A] …
     uint8* const topRawImageData = static_cast<uint8*>(topImageData->Lock(LOCK_READ_WRITE));
     const int32 step = 4;
-    const int32 alphaOffset = 3;
     for (int32 i = 0; i < firstHalf; ++i) {
-        topRawImageData[i * step] = 255u;
-        topRawImageData[i * step + alphaOffset] = 255u;
+        const int32 blue = step * i;
+        topRawImageData[blue] = 255u;
+        topRawImageData[blue + 1] = 0u;
+        topRawImageData[blue + 2] = 0u;
+        topRawImageData[blue + 3] = 255u;
     }
+    for (int32 i = firstHalf; i < nPixels; ++i) {
+        const int32 blue = step * i;
+        topRawImageData[blue] = 0u;
+        topRawImageData[blue + 1] = 0u;
+        topRawImageData[blue + 2] = 0u;
+        topRawImageData[blue + 3] = 0u;
+    }
+
     topImageData->Unlock();
     topTexture->UpdateResource();
     _materialInstance->SetTextureParameterValue(FName("TopTexture"), topTexture);
 
     // This texture has B = 1 for the second half. We don't give a shit about the other values, this is just to mask the color for a given region.
     UTexture2D* const bottomTexture = UTexture2D::CreateTransient(_width, _height);
+
+    // bottomTexture->MipGenSettings = TextureMipGenSettings::TMGS_NoMipmaps;
+    // bottomTexture->GetTextureGroupString useless?
+    bottomTexture->Filter = TextureFilter::TF_Nearest;
+
     FTexture2DMipMap* const bottomMipMap = &bottomTexture->PlatformData->Mips[0];
     FByteBulkData* const bottomImageData = &bottomMipMap->BulkData;
     const int64 bottomSize = bottomImageData->GetBulkDataSize();
     // RawImageData is formatted as such:
     // [Pixel1 B][Pixel1 G][Pixel1 R][Pixel1 A][Pixel2 B][Pixel2 G][Pixel2 R][Pixel2 A] …
     uint8* const bottomRawImageData = static_cast<uint8*>(bottomImageData->Lock(LOCK_READ_WRITE));
-    for (int32 i = firstHalf; i < nPixels; ++i) {
-        bottomRawImageData[i * step] = 255u;
-        bottomRawImageData[i * step + alphaOffset] = 255u;
+    for (int32 i = 0; i < firstHalf; ++i) {
+        const int32 blue = step * i;
+        bottomRawImageData[blue] = 0u;
+        bottomRawImageData[blue + 1] = 0u;
+        bottomRawImageData[blue + 2] = 0u;
+        bottomRawImageData[blue + 3] = 0u;
     }
+    for (int32 i = firstHalf; i < nPixels; ++i) {
+        const int32 blue = step * i;
+        bottomRawImageData[blue] = 255u;
+        bottomRawImageData[blue + 1] = 0u;
+        bottomRawImageData[blue + 2] = 0u;
+        bottomRawImageData[blue + 3] = 255u;
+    }
+
     bottomImageData->Unlock();
     bottomTexture->UpdateResource();
     _materialInstance->SetTextureParameterValue(FName("BottomTexture"), bottomTexture);
