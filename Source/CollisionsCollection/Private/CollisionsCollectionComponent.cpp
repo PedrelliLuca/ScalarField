0ad33776f0251196d8fa2a461f3fc7be7b506ccc
Luca Pedrelli <pedrelli.luca@gmail.com>
2024-04-20 18:41:52 +0100

    UCollisionsCollectionComponent::BeginPlay()

--- Source/CollisionsCollection/Private/CollisionsCollectionComponent.cpp
+++ Source/CollisionsCollection/Private/CollisionsCollectionComponent.cpp
@@ -2,5 +2,69 @@
 
 #include "CollisionsCollectionComponent.h"
 
-UCollisionsCollectionComponent::UCollisionsCollectionComponent() {
+#include "Components/SphereComponent.h"
+
+// https://stackoverflow.com/questions/14421656/is-there-widely-available-wide-character-variant-of-file
+#define WIDE2(X) L##X
+#define WIDE1(X) WIDE2(X)
+#define WFUNCTION WIDE1(__FUNCTION__)
+
+UCollisionsCollectionComponent::UCollisionsCollectionComponent()
+    : _collectionTag("")
+    , _collectionCollisionProfileName("")
+    , _collectionSpheres()
+    , _overlappingElements(0) {
+    PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UCollisionsCollectionComponent::BeginPlay() {
+    Super::BeginPlay();
+
+    _collectSpheres();
+}
+
+void UCollisionsCollectionComponent::_collectSpheres() {
+    const TArray<UActorComponent*> sphereActors = GetOwner()->GetComponentsByTag(USphereComponent::StaticClass(), _collectionTag);
+    _collectionSpheres.Reserve(sphereActors.Num());
+
+    for (auto sphereActor : sphereActors) {
+        const auto sphere = Cast<USphereComponent>(sphereActor);
+        check(sphere);
+
+        const auto collisionProfile = sphere->GetCollisionProfileName();
+        if (collisionProfile != _collectionCollisionProfileName) {
+            UE_LOG(LogTemp, Warning, WFUNCTION TEXT(" Collision tagged as %s has Profile %s instead of %s, replacing."), *_collectionTag.ToString(),
+                *collisionProfile.ToString(), *_collectionCollisionProfileName.ToString());
+
+            sphere->SetCollisionProfileName(_collectionCollisionProfileName);
+        }
+
+        const bool generateOverlapEvents = sphere->GetGenerateOverlapEvents();
+        if (generateOverlapEvents != _generateOverlapEvents) {
+            const auto collisionString = generateOverlapEvents ? TEXT("true") : TEXT("false");
+            const auto collectionString = _generateOverlapEvents ? TEXT("true") : TEXT("false");
+            UE_LOG(LogTemp, Warning, WFUNCTION TEXT(" Collision has \"generate overlap events\" set to %s instead of %s, replacing."), collisionString,
+                collectionString);
+
+            sphere->SetGenerateOverlapEvents(_generateOverlapEvents);
+        }
+
+        FCollectionSphereParameters sphereParameters;
+        sphereParameters.RootRelativeLocation = FVector2D(sphere->GetComponentTransform().GetLocation());
+        sphereParameters.Radius = sphere->GetScaledSphereRadius();
+        _collectionSpheres.Add(MoveTemp(sphereParameters));
+
+        sphere->OnComponentBeginOverlap.AddDynamic(this, &UCollisionsCollectionComponent::_collectionElementBeginOverlap);
+        sphere->OnComponentEndOverlap.AddDynamic(this, &UCollisionsCollectionComponent::_collectionElementEndOverlap);
+    }
+}
+
+void UCollisionsCollectionComponent::_collectionElementBeginOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor,
+    UPrimitiveComponent* otherComp, int32 otherBodyIndex, bool bFromSweep, const FHitResult& sweepResult) {
+    // If _overlappingElements was 0 fire event saying collection is now overlapping. Then, increase _overlappingElements.
+}
+
+void UCollisionsCollectionComponent::_collectionElementEndOverlap(
+    UPrimitiveComponent* overlappedComponent, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex) {
+    // Decrease _overlappingElements. If counter is now 0 fire event saying collection is not overlapping anymore
 }
--- Source/CollisionsCollection/Public/CollisionsCollectionComponent.h
+++ Source/CollisionsCollection/Public/CollisionsCollectionComponent.h
@@ -7,8 +7,15 @@
 #include "Components/ActorComponent.h"
 #include "CollisionsCollectionComponent.generated.h"
 
+DECLARE_MULTICAST_DELEGATE_SixParams(FCollectionBeginOverlapSignature, UPrimitiveComponent*, AActor*, UPrimitiveComponent*, int32, bool, const FHitResult&);
+
+struct FCollectionSphereParameters {
+    FVector2D RootRelativeLocation = FVector2D::ZeroVector;
+    float Radius = 0.0f;
+};
+
 /**
- * \brief TODO
+ * \brief Collects Sphere Collisions with a given tag so that they can act as one for other components.
  */
 UCLASS(ClassGroup = "Collision", meta = (BlueprintSpawnableComponent))
 class COLLISIONSCOLLECTION_API UCollisionsCollectionComponent : public UActorComponent {
@@ -16,4 +23,38 @@ class COLLISIONSCOLLECTION_API UCollisionsCollectionComponent : public UActorCom
 
 public:
     UCollisionsCollectionComponent();
+
+protected:
+    void BeginPlay() override;
+
+private:
+    void _collectSpheres();
+    // TODO: _collectCapsules(), _collectBoxes()
+
+    UFUNCTION()
+    void _collectionElementBeginOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex,
+        bool bFromSweep, const FHitResult& sweepResult);
+
+    UFUNCTION()
+    void _collectionElementEndOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex);
+
+    /*void _beginCollectionOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex,
+        bool bFromSweep, const FHitResult& sweepResult);*/
+
+    // The Tag the collisions must have to be registered by the collection.
+    UPROPERTY(EditAnywhere, Category = "Collection Parameters")
+    FName _collectionTag;
+
+    // The Collision Profile the tagged collisions must have to be registered by the collection.
+    UPROPERTY(EditAnywhere, Category = "Collection Parameters")
+    FName _collectionCollisionProfileName;
+
+    // Whether the members of the collection should generate overlap events.
+    UPROPERTY(EditAnywhere, Category = "Collection Parameters")
+    bool _generateOverlapEvents;
+
+    TArray<FCollectionSphereParameters> _collectionSpheres;
+
+    // The number of elements in the collection which are currently overlapping
+    int32 _overlappingElements;
 };
