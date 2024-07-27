4929fc3670f5415cb6566898c7e89194ca070f6f
Luca Pedrelli <pedrelli.luca@gmail.com>
2024-07-27 18:31:43 +0100

    Creatiton of widgets c++ classes

---
+++ Source/GameplayUI/Public/SkillIconContainerWidget.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "CoreMinimal.h"
+
+#include "SkillIconContainerWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS(Blueprintable)
+class GAMEPLAYUI_API USkillIconContainerWidget : public UUserWidget {
+    GENERATED_BODY()
+
+public:
+};
